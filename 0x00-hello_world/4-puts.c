036819a50b15a3482561416490964ea97a881288
Emmanuel Gyabaah <gyabemman1@gmail.com>
2022-12-11 22:05:26 -0800

    Fix for Betty style documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- * Prints a line and returns 0
+ * main - Entry point
+ * Prints a line 
+ *
+ * Return:0
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
